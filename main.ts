4d248c89715263ca2eb2a8bd4d5bbf3e30c5c448
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-10-20 13:29:24 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let randomNumberOne: number = randint(0, 99)
+let randomNumberTwo: number = randint(0, 99)
+
+// Clean up
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+  basic.showNumber(randomNumberOne)
+})
+
+input.onButtonPressed(Button.B, function() {
+  basic.showNumber(randomNumberTwo)
+})
+
+input.onGesture(Gesture.Shake, function() {
+  
+if (true) {
+    
+} else {
+    
+}
+})  
