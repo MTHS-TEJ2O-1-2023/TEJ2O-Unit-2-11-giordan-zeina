a4eb60a2b2f26ec77ede502e18a0471da993fa49
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-10-24 13:09:22 -0400

    type

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
+ * Created by: Giordan Zeina
  * Created on: Oct 2023
- * This program ...
+ * This program on "start", generate 2 random integers from 0 to 99
+ * on button "A", show the 1st number
+ * on button "B", show the 2nd number
+ * on "shake", show either:
+ * 1st < 2nd
+ * 1st > 2nd
 */
 
 let randomNumberOne: number = randint(0, 99)
@@ -12,19 +17,26 @@ let randomNumberTwo: number = randint(0, 99)
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  basic.showString('#1:')
   basic.showNumber(randomNumberOne)
 })
 
-input.onButtonPressed(Button.B, function() {
+input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
+  basic.showString('#2:')
   basic.showNumber(randomNumberTwo)
 })
 
-input.onGesture(Gesture.Shake, function() {
-  
-if (true) {
-    
-} else {
-    
+input.onGesture(Gesture.Shake, function () {
+  if (randomNumberOne < randomNumberTwo) {
+    basic.showNumber(randomNumberOne)
+    basic.showString('<')
+    basic.showNumber(randomNumberTwo)
+  } else {
+    basic.showNumber(randomNumberOne)
+    basic.showString('>')
+    basic.showNumber(randomNumberTwo)
 }
-})  
+})
